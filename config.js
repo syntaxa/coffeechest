f0f17244370df9c818b080b7cd6a7dc46fd654e3
syntaxa <30318815+syntaxa@users.noreply.github.com>
2026-03-03 23:54:19 +0300

    Update GEMINI_MODEL_NAME to gemini-3-flash-preview

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const botConfig = {
     TIMEZONE: process.env.TIMEZONE || 'Europe/Moscow',
     WIN_MESSAGE: process.env.WIN_MESSAGE || 'Поздравляю! Тебе выпало кофечко сегодня! 🎉',
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    GEMINI_MODEL_NAME: process.env.GEMINI_MODEL_NAME || 'gemini-2.0-flash',
+    GEMINI_MODEL_NAME: process.env.GEMINI_MODEL_NAME || 'gemini-3-flash-preview',
     GEMINI_TEMPERATURE: parseFloat(process.env.GEMINI_TEMPERATURE) || 1.5,
     GEMINI_MAX_OUTPUT_TOKENS: parseInt(process.env.GEMINI_MAX_OUTPUT_TOKENS) || 300,
     GEMINI_PROMPT: "Придумай две темы, связанные с природой. Напиши хайку на русском языке в классическом стиле, с образной, метафорической связностью, без юмора. Главная тема это кофе, но также добавь к контексту темы, придуманные ранее. Хайку должно отражать атмосферу спокойствия, тепла и утреннего настроения, как в традиционной японской поэзии. Обязательно затронь тему кофе. в ответе не должно быть тем, а должно быть только текст хайку. очень важно: проверь ответ и убери из него список тем. оставь только хайку.",

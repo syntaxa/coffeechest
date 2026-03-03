13f71d7a1deffc5824dd72320925c553e11ad660
syntaxa <30318815+syntaxa@users.noreply.github.com>
2026-03-03 23:41:57 +0300

    Update GEMINI_PROMPT for haiku generation

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const botConfig = {
     GEMINI_MODEL_NAME: process.env.GEMINI_MODEL_NAME || 'gemini-2.0-flash',
     GEMINI_TEMPERATURE: parseFloat(process.env.GEMINI_TEMPERATURE) || 1.5,
     GEMINI_MAX_OUTPUT_TOKENS: parseInt(process.env.GEMINI_MAX_OUTPUT_TOKENS) || 300,
-    GEMINI_PROMPT: "придумай три темы для стихов. придумай один стих в стиле хайку про кофе с тонким юмором и используй придуманные ранее темы для вдохновения. ответ должен содержать только текст хайку. убери темы из результата.",
+    GEMINI_PROMPT: "Придумай две темы, связанные с природой. Напиши хайку на русском языке в классическом стиле, с образной, метафорической связностью, без юмора. Главная тема это кофе, но также добавь к контексту темы, придуманные ранее. Хайку должно отражать атмосферу спокойствия, тепла и утреннего настроения, как в традиционной японской поэзии. Обязательно затронь тему кофе. в ответе не должно быть тем, а должно быть только текст хайку. очень важно: проверь ответ и убери из него список тем. оставь только хайку.",
     ADMIN_CHAT_ID: process.env.ADMIN_CHAT_ID,
     ENVIRONMENT: process.env.ENVIRONMENT || 'PROD',
     TESTING_CHAT_ID: process.env.TESTING_CHAT_ID
